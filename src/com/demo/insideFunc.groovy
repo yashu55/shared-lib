58251df64430ab8ee62bddf11bac863b437ea0d4
yashu55 <ysp551996@gmail.com>
2021-06-21 13:38:24 +0530

    advsdv

--- src/com/demo/insideFunc.groovy
+++ src/com/demo/insideFunc.groovy
@@ -3,6 +3,26 @@ package com.demo
 
 public class insideFunc{
     def hello(){
-        println("Hello")
+       pipeline {
+		agent any
+
+		stages {
+			stage('wtrt') {
+				steps {
+					echo 'Hello dgdfgdfg'
+				}
+			}
+			stage('dwetwtef') {
+				steps {
+					echo 'fggh'
+				}
+			}
+			stage('hiwewetj') {
+				steps {
+					echo 'Hello'
+				}
+			}
+		}
+	}
     }
 }
--- vars/demoFunc.groovy
+++ vars/demoFunc.groovy
@@ -4,27 +4,7 @@ import com.demo.insideFunc
 def call(){
 	def helloVar = new insideFunc()
 	helloVar.hello()
-	pipeline {
-		agent any
-
-		stages {
-			stage('wtrt') {
-				steps {
-					echo 'Hello dgdfgdfg'
-				}
-			}
-			stage('dwetwtef') {
-				steps {
-					echo 'fggh'
-				}
-			}
-			stage('hiwewetj') {
-				steps {
-					echo 'Hello'
-				}
-			}
-		}
-	}
+	
 }
 
 // def functionDemo(){
