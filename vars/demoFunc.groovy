b2edd21769f80c43f92c091fba3577446823a260
yashu55 <ysp551996@gmail.com>
2021-06-21 11:34:06 +0530

    Lib

---
+++ vars/demoFunc.groovy
@@ -0,0 +1,24 @@
+
+def call(){
+	pipeline {
+		agent any
+
+		stages {
+			stage('abc') {
+				steps {
+					echo 'Hello dgdfgdfg'
+				}
+			}
+			stage('def') {
+				steps {
+					echo 'Hellodfgdg'
+				}
+			}
+			stage('hij') {
+				steps {
+					echo 'Hello'
+				}
+			}
+		}
+	}
+}
