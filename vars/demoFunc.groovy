8a0a7efea3c643c5e405ca56fe40f659df917352
yashu55 <ysp551996@gmail.com>
2021-06-22 15:00:18 +0530

    wgergerg

--- vars/demoFunc.groovy
+++ vars/demoFunc.groovy
@@ -5,21 +5,24 @@ def  call(){
 		parameters {
 			choice(name: 'CHOICE', choices: ['ANSIBLE', 'TRANSPORTER', 'SCP'], description: 'Pick something')
     	}
+		environment { 
+        	CHOICE = 'TRANSPORTER'
+    	}
 		stages {
 			stage('ANSIBLE') {
-				when { equals expected: 'ANSIBLE', actual: params.CHOICE }
+				when { equals expected: 'ANSIBLE', actual: env.CHOICE }
 				steps{
 					echo "Ansible"
 				}
 			}
 			stage('TRANSPORTER') {
-				when { equals expected: 'TRANSPORTER', actual: params.CHOICE }
+				when { equals expected: 'TRANSPORTER', actual: env.CHOICE }
 				steps{
 					echo "Transporter"
 				}
 			}
 			stage('SCP') {
-				when { equals expected: 'SCP', actual: params.CHOICE }
+				when { equals expected: 'SCP', actual: env.CHOICE }
 				steps{
 					echo "SCP"
 				}
