dbc27483da39f3bbe8c04a28e9956be38d32c37d
yashu55 <ysp551996@gmail.com>
2021-06-23 14:28:15 +0530

    dfgdfg

--- vars/demoFunc.groovy
+++ vars/demoFunc.groovy
@@ -1,26 +1,7 @@
 
 def  call(){
 	
-		stages{
-			stage('ANSIBLE') {
-				when { equals expected: 'ANSIBLE', actual: env.CHOICE }
-				steps{
-					echo "Ansible"
-				}
-			}
-			stage('TRANSPORTER') {
-				when { equals expected: 'TRANSPORTER', actual: env.CHOICE }
-				steps{
-					echo "Transporter"
-				}
-			}
-			stage('SCP') {
-				when { equals expected: 'SCP', actual: env.CHOICE }
-				steps{
-					echo "SCP"
-				}
-			}
-		}
+		echo "SCP"
 	
 }
 
