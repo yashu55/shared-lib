9b93e7921f7372601ddc9670d7df0acaf29e5452
yashu55 <ysp551996@gmail.com>
2021-06-25 18:34:43 +0530

    hjmhjmhjm

--- vars/demoFunc.groovy
+++ vars/demoFunc.groovy
@@ -1,35 +1,12 @@
 
 def  call(Map pipelineParams){
 	node {
-	
-
 			stage("Init"){
-
-				steps{      
-					script{
-					pipelineParams.type = pipelineParams.type?.trim().toUpperCase()
-					if( pipelineParams.type != 'ANSIBLE' && pipelineParams.type != 'TRANSPORTER' && pipelineParams.type != 'SCP' ){
-						error("type is null")
-					}
-					}
-				}
+						echo "Hello"
 			}
 
 			stage("Hi") {
-				// when{
-				// 	allOf {
-				// 	equals expected: "ANSIBLE", actual: pipelineParams.type
-				// 	expression { !pipelineParams.path || pipelineParams.path == 'none'}
-				// 	}
-				// }
-				steps{      
-					script{
-						echo pipelineParams.type
-						echo pipelineParams.path
-
-					}
-				}
-				
+						echo "Hi"	
 			}	
 		
 	}
