9a62178b384f0638f5521d9e08a058c4d5f95c14
yashu55 <ysp551996@gmail.com>
2021-06-30 13:30:34 +0530

    geg

--- vars/demoFunc.groovy
+++ vars/demoFunc.groovy
@@ -4,6 +4,9 @@
 def  call(Map pipelineParams){
 	def request = libraryResource 'script.sh'
 	writeFile(file: "script.sh", text: request)
+	sh "ls -la"
+	sh "chmod 777 script.sh"
+    sh "./script.sh"
 }
 
 
