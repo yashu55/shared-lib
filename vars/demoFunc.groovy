213f9dba73eb335398a21838573e4a06bf5019dc
yashu55 <ysp551996@gmail.com>
2021-06-30 13:22:28 +0530

    sgerg

--- vars/demoFunc.groovy
+++ vars/demoFunc.groovy
@@ -1,56 +1,9 @@
 
-@Library('shared-lib@master') _
 
 
 def  call(Map pipelineParams){
-	node  {
-		properties([
-        	buildDiscarder(
-                logRotator(
-                        artifactDaysToKeepStr: '', 
-                        artifactNumToKeepStr: '', 
-                        daysToKeepStr: '', 
-                        numToKeepStr: '')
-        	), 
-        	disableConcurrentBuilds()
-		])
-		echo "SCM Checkout done"
-		echo env.NODE_NAME
-		echo env.NODE_LABELS
-
-	}
-	// if(false)
-	// 	println("byeeeeeeeeeee")
-	// else
-	// 	error("errrrorrrrr")
-			// stage("Init"){
-			// 	node{
-			// 		checkout scm
-			// 		script{
-			// 		echo "Heldfsgdfglo"
-			// 		}
-					
-			// 	}
-			// }
-
-			// stage("Hi") {
-			// 	when {equals expected: "Hello", actual: "Hello"}
-			// 	echo "sgfg"		
-			// }	
-	//}
-	// node  {
-			
-	// 		stage("fgdfg"){
-	// 			withCredentials([usernameColonPassword(credentialsId: 'user_pass', variable: 'user')]) {
-	// 				echo user
-	// 				}
-	// 		}
-
-	// 		stage("dfgdg") {
-				
-	// 			echo "Hi"		
-	// 		}	
-	// }
+	def request = libraryResource 'script.sh'
+	writeFile(file: "script.sh", text: request)
 }
 
 
---
+++ vars/resources/script.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+echo "What is your name?"
