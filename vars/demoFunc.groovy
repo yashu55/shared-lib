3b2bb519faa2586c5bbb4efab1275e3f0ff405cb
yashu55 <ysp551996@gmail.com>
2021-07-19 23:34:47 +0530

    lib changes

--- vars/demoFunc.groovy
+++ vars/demoFunc.groovy
@@ -2,11 +2,13 @@
 
 
 def  call(Map pipelineParams){
-	def request = libraryResource 'configValidationYAML/script.sh'
-	writeFile(file: "script.sh", text: request)
-	sh "ls -la"
-	sh "chmod 777 script.sh"
-    sh "./script.sh"
+	// def request = libraryResource 'configValidationYAML/script.sh'
+	// writeFile(file: "script.sh", text: request)
+	// sh "ls -la"
+	// sh "chmod 777 script.sh"
+    // sh "./script.sh"
+
+	sh "echo I_am_shared_lib"
 }
 
 
